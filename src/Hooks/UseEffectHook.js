b83b822ed9c300fed5dfc1f7cf167a57837b913b
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-08-24 16:46:47 +0500

    use UseEffect Hook

--- src/Hooks/UseEffectHook.js
+++ src/Hooks/UseEffectHook.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseEffectHook = () => {
+
+    const [resourceType, setResourceType] = useState('Posts')
+
+    useEffect(() => {
+        alert(`Resource type changed to ${resourceType}`)
+
+        return () => {
+            alert('')
+        }
+    }, [resourceType])
+
   return (
-    <div>UseEffectHook</div>
+    <>
+        <button onClick={() => setResourceType('Posts')}>Posts</button>
+        <button onClick={() => setResourceType('Users')}>Users</button>
+        <button onClick={() => setResourceType('Comments')}>Comments</button>
+    </>
   )
 }
 
