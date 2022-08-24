62a2c6e8556220f76fe2397289d3398340329ef0
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-08-24 16:12:28 +0500

    add useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import UseStateHook from './Hooks/UseStateHook'
+import UseEffectHook from './Hooks/UseEffectHook'
+
 function App() {
   return (
     <div>
-      <UseStateHook />
+      {/* <UseStateHook /> */}
+      <UseEffectHook />
     </div>
   );
 }
---
+++ src/Hooks/UseEffectHook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UseEffectHook = () => {
+  return (
+    <div>UseEffectHook</div>
+  )
+}
+
+export default UseEffectHook
