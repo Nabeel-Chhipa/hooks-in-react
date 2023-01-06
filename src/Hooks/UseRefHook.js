00c64afd07483c697afaa5d53054787c9209f171
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-01-06 22:45:08 +0500

    useRef hook intro

--- src/Hooks/UseRefHook.js
+++ src/Hooks/UseRefHook.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const UseRefHook = () => {
+
+  const inputRef = useRef(null)
+
+  const handleFunction = () => {
+    // console.log('Testing')
+    inputRef.current.value='button click'
+    inputRef.current.focus()
+  }
+
   return (
-    <div>UseRefHook</div>
+    <>
+      <h4>React useRef Hook.</h4>
+      <input type='text' ref={inputRef} />
+      <button onClick={handleFunction}>Click Here</button>
+    </>
   )
 }
 
